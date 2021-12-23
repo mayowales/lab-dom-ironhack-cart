a0e556af99df04651294534fc76e8169cef9f6ae
Wale <walekolade45@gmail.com>
2021-12-23 01:43:38 +0100

    remaining bonus

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 // ITERATION 1
 
+//const { product } = require("puppeteer");
+
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+const price = product.querySelector(".price span").innerHTML
+const quantity = product.querySelector(".quantity input").value
+
+const subTotal = price * quantity
+product.querySelector(".subtotal span").innerHTML = subTotal
+
+
+return subTotal;
+//console.log("quantity",quantity)
+
+
+  
+  //console.log('Calculating subtotal, yey!');
 
   //... your code goes here
 }
@@ -9,15 +23,27 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
+  
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
+  
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  let productCart = [...document.getElementsByClassName('product')];
+  console.log(productCart)
+  let totalPrice = 0;
+ productCart.forEach(productRow => {
+   totalPrice += updateSubtotal(productRow)
+ });
+  
+ console.log(totalPrice)
 
   // ITERATION 3
   //... your code goes here
+
+  document.querySelector('#total-value span').innerHTML = totalPrice
 }
 
 // ITERATION 4
@@ -26,6 +52,11 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  let remove = [...document.getElementsByClassName('btn btn-remoev')];
+  remove.forEach(button => {
+    remove.addEventListener('click', removeProduct);
+  })
+
 }
 
 // ITERATION 5
